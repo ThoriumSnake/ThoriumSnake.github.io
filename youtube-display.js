df342c64dd63ced5f3daae53b59cfe426aad6626
thoriumsnake <thoriumsnake@outlook.com>
2023-08-27 19:27:08 -0300

    Pagination buttons working

--- youtube-display.js
+++ youtube-display.js
@@ -1,3 +1,5 @@
+//Try it the shitty way, think about performance later
+//Array of all videos, shift 'em, make brand new pages from that
 
 var playerDiv = document.getElementById("player");
 var testButton = document.getElementById("test-button").addEventListener("click", (e) => {
@@ -9,15 +11,20 @@ var testButton = document.getElementById("test-button").addEventListener("click"
 var paginationContainer = document.getElementsByClassName("page-number-cont")[0];
 
 var pageIndex = 1;
+var currentPageElement;
+var pages = [];
 
 export function createFirstPage(videos) {
     const panelContainer = document.getElementById("videoList");
 
+    paginationContainer.innerHTML = "";
     panelContainer.innerHTML = ""; // Clear the video list if already present
     player.cueVideoById({
         "videoId": videos[0].snippet.resourceId.videoId,
     })
     createPlaylistPage(videos, 1);
+    currentPageElement = pages[0];
+    currentPageElement.style.display = "block";
 }
 
 export function createPlaylistPage(videos, index) {
@@ -27,6 +34,7 @@ export function createPlaylistPage(videos, index) {
     const panelContainer = document.getElementById("videoList");
 
     var pageContainer = document.createElement("div");
+    pageContainer.dataset.pageNumber = index;
     pageContainer.classList.add("playlist-page-cont");
 
     for (const video of videos) {
@@ -49,17 +57,32 @@ export function createPlaylistPage(videos, index) {
         pageContainer.appendChild(itemContainer);
     }
 
-    panelContainer.appendChild(pageContainer);
-    createPaginationButton(index);
+    pageContainer.style.display = "none";
+    createPageButton(index);
+    pages.push(panelContainer.appendChild(pageContainer));
 }
 
-function createPaginationButton(index) {
+function createPageButton(index) {
     var pageButton = document.createElement("li");
     pageButton.dataset.pageNumber = index;
     pageButton.textContent = index;
     pageButton.classList.add("page-button")
+    pageButton.addEventListener("click", (event) => {
+        setPage(event.target.dataset.pageNumber);
+    })
 
     paginationContainer.appendChild(pageButton);
+}
+
+function setPage(index) {
+    currentPageElement.style.display = "none";
+
+    pageIndex = index;
+    currentPageElement = pages[index - 1]
+
+    currentPageElement.style.display = "block";
+    console.log(pages);
+    console.log(currentPageElement);
 }
 
 function setVideo() {
